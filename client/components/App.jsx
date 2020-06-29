7b4d7045c821a734a564335d50a99385230ab65e
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-06-30 11:04:48 +1200

    add header

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-// This might need to be turned into a stateful (class-based) component
-const App = () => (
-  <div className='app'>
-    Ready to rock and roll
-  </div>
-)
+import Header from './Header'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='app'>
+        < Header />
+      </div>
+    )
+  }
+}
 
 export default App
---
+++ client/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render () {
+    return (
+      <>
+          <h1><span className="fa fa-beer"></span> Sweet As Beers <span className="fa fa-beer"></span></h1>
+
+      </>
+    )
+  }
+}
+
+export default Header
