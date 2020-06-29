6d54414b8e158079a5f0a1d569bef1698c9d2a56
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-06-30 11:24:26 +1200

    header component that renders

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
+  <>
   <div className='app'>
-    Ready to rock and roll
+    <Header />
   </div>
+  </>
 )
 
 export default App
---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => (
+  <>
+    <h1><span className="fa fa-beer"></span> Sweet As Beers <span className="fa fa-beer"></span></h1>
+
+    <p className="welcome">Welcome! Please select from our delicious selection and don't hesitate to let us know if we can answer any of your questions.</p>
+  </>
+)
+
+export default Header
