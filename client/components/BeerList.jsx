d6331e928d884115058a5d0094400eae3f02f7f3
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-30 16:18:59 +1200

    got add to cart button working

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -7,7 +7,7 @@ const Beerlist = (props) => (
     <h1><span className="fa fa-beer"></span> Sweet As Beers <span className="fa fa-beer"></span></h1>
     <p className="welcome">Welcome! Please select from our delicious selection and don't hesitate to let us know if we can answer any of your questions.</p>
     {props.beerData.beers.map(beer =>
-      <BeerListItem key={beer.id} name={beer.name} description={beer.description} country={beer.country} abv={beer.abv} />
+      <BeerListItem key={beer.id} name={beer.name} description={beer.description} country={beer.country} abv={beer.abv} id={beer.id}/>
     )}
   </div>
 )
--- client/reducers/beers.js
+++ client/reducers/beers.js
@@ -5,7 +5,7 @@ const initialState = {
 function beers (state = initialState, action) {
   switch (action.type) {
     case 'ADD_BEER_TO_CART':
-      state.cartBeers.push(action.beer)
+      state.cartBeers.push(action.beerid)
       return state
     default:
       return state
