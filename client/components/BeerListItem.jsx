a9e724e90260d9ae4be140b6b5c27761d59de2cb
Ariel <33848430+arielwc@users.noreply.github.com>
2020-07-01 15:03:55 +1200

    changed cart.jsx to class-based component

--- client/components/BeerListItem.jsx
+++ client/components/BeerListItem.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const BeerListItem = (props) => {
-  return (
-    <div>
-      <div className="beer">
-        <p className="name">{props.beerprop.name}</p>
-        <p className="description">A {props.beerprop.style} from {props.beerprop.brewery}</p>
-        <p>
-          <span className="country">{props.beerprop.country}</span>
-          <span className="abv">{props.beerprop.abv}</span>
-          <a href="/designs/cart.html" className="cart-link">Add to cart</a>
-        </p>
-      </div>
-    </div>
-  )
-}
+class BeerListItem extends React.Component {
+  handleClick = () => {
+    const action = {
+      type: 'NAVIGATION',
+      navigation: this.state.navigation
+    }
 
+    this.props.store.dispatch(action)
+  }
+  render (props) {
+    return (
+      <div>
+        <div className="beer">
+          <p className="name">{props.beerprop.name}</p>
+          <p className="description">A {props.beerprop.style} from {props.beerprop.brewery}</p>
+          <p>
+            <span className="country">{props.beerprop.country}</span>
+            <span className="abv">{props.beerprop.abv}</span>
+
+            {/* create onclick function for navigation */}
+            <button onClick={this.handleClick}>Add to cart</button>
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
 // const mapStateToProps = (state) => ({
 
 // })
--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import data from '../../data/beers'
 import CartListItem from './CartListItem'
 
-export const Cart = () => {
-  return (
-    <div>
-      <p className="welcome">Thirsty? Sweet! You&apos;re one step closer to a quenching.</p>
+class Cart extends React.Component {
+  handleClick = () => {
+    const action = {
+      type: 'NAVIGATION',
+      navigation: this.state.navigation
+    }
+
+    this.props.store.dispatch(action)
+  }
+
+  render () {
+    return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Beer</th>
-              <th>Quantity</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            <CartListItem />
-          </tbody>
+        <p className="welcome">Thirsty? Sweet! You&apos;re one step closer to a quenching.</p>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Beer</th>
+                <th>Quantity</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              <CartListItem />
+            </tbody>
 
-        </table>
+          </table>
 
-        <p className="actions">
-          <a href='#'>Continue shopping</a>
-          <a href='#'><button>Update</button></a>
-          <a href='#'><button className="button-primary">Checkout</button></a>
-        </p>
+          {/* create onclick function for navigation */}
+
+          <p className="actions">
+            <button onClick={this.handleClick}>Continue shopping</button>
+            <button>Update</button>
+            <button className="button-primary">Checkout</button>
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 // const mapStateToProps = (state) => ({
