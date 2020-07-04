2c8d31b794552aae6a34bab784daee977700f571
Kim Fitter <kimfitter@yahoo.com>
2020-07-04 14:56:39 +1200

    Rerun migration and seed on table beersies

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-
 const knex = require('knex')
 const config = require('./knexfile').development
 db = knex(config)
 
 module.exports = {
-    getBeers,
+  getBeers
 }
 
 // New function to select all beers from table beersies, as an array
-function getBeers() ={
-    return db('beersies').select()
+function getBeers () {
+  return db('beersies')
+    .select()
 }
--- routes/beerroutes.js
+++ routes/beerroutes.js
@@ -5,14 +5,12 @@ const express = require('express')
 const router = express.Router()
 module.exports = router
 
-// Create a route that will run the getBeers query on the database and render as a 'view'. 
+// Create a route that will run the getBeers query on the database and render as a 'view'.
 // This 'view is tbc how to do with React?
 router.get('/find', (req, res) => {
-    db.getBeers()
-        .then(beers => {
-            const viewData = { beers }
-            res.render('view', viewData)
-        })
-
-
+  db.getBeers()
+    .then(beers => {
+      const viewData = { beers }
+      res.render('view', viewData)
+    })
 })
