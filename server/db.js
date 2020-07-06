bad8b7fb428a261531f0e0edb04a49d7688ebd48
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-07-06 16:30:57 +1200

    fixed function getData, removed unsuable const

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const { default: beers } = require('../data/beers')
+// const { beers } = require('../data/beers')
 
 const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
@@ -13,12 +13,17 @@ module.exports = {
 function getData (db = connection) {
   return db('beers')
     .select(
-      'id'
-
+      'id',
+      'name',
+      'brewery',
+      'country',
+      'style',
+      'abv'
     )
 }
 
-function addBeer (beer, db = connection) {
+// beers on line 26 might need to be changed to 'beer'
+function addBeer (beers, db = connection) {
   return db('beers')
     .insert({
       name: beers.name,
@@ -35,7 +40,7 @@ function addBeer (beer, db = connection) {
     })
 }
 
-function updateBeer (beer, id, db = connection) {
+function updateBeer (beers, id, db = connection) {
   return db('beers')
     .where('id', id)
     .update({
