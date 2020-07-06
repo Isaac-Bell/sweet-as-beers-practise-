cbfd3bde0e2d673b93f1eca2d5f92687d60f326f
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-07-06 16:23:04 +1200

    api.js file added to grab data from database

---
+++ client/api.js
@@ -0,0 +1,12 @@
+import request from 'superagent'
+
+const dataUrl = 'http://localhost:3000/api/v1'
+
+export function getData () {
+  return request
+    .get(dataUrl)
+    .then(response => {
+      console.log('client-side: ', response.body)
+      return response.body
+    })
+}
